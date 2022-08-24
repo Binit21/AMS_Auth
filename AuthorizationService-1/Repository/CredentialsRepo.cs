127686c9f90f1909c2b57fcdd55681a6838ec57e
Binit21 <111643989+Binit21@users.noreply.github.com>
2022-08-24 10:11:54 +0530

    Add files via upload
    Auth commit

---
+++ AuthorizationService-1/Repository/CredentialsRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationService.Repository
+{
+    public class CredentialsRepo: ICredentialsRepo
+    {
+        private Dictionary<string, string> ValidUsersDictionary = new Dictionary<string, string>()
+        {
+               {"admin","admin"},
+               {"binit","binit"}
+        };
+        public Dictionary<string,string> GetCredentials()
+        {
+            return ValidUsersDictionary;
+        }
+    }
+}
